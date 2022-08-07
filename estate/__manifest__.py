b9f294f89f1dc4542c87ef5e3eb3a9fc6326a6af
Sergio Zavala <sergio.zavala@nubosoft.com>
2022-08-07 21:36:59 +0000

    Seguridad

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,8 @@
 {
-'name': 'Real State',
-'author': 'Laboratorios Implementacion Odoo',
-"application": True,
-
+    'name': 'Real State',
+    'author': 'Laboratorios Implementacion Odoo',
+    "data": [
+            "security/ir.model.access.csv"
+    ],
+    "application": True,
 }
