e1307aebc7e4e1e7be08c6894f7f97ee8ae0bd97
Sergio Zavala <sergio.zavala@nubosoft.com>
2022-08-08 23:49:53 +0000

    One2many

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,7 @@
     'author': 'Laboratorios Implementacion Odoo',
     "data": [
             "security/ir.model.access.csv",
+            "views/estate_property_offer_views.xml",
             "views/estate_property_tag_views.xml",
             "views/estate_property_type_views.xml",
             "views/estate_property_views.xml",
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -60,3 +60,6 @@ class EstateProperty(models.Model):
     # Many2Many
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
 
+    # One2many
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+
