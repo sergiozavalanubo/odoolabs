0ed13b9e17367158986345e1911b90d4e5782810
Sergio Zavala <sergio.zavala@nubosoft.com>
2022-08-08 22:06:39 +0000

    Many2one

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,6 +3,7 @@
     'author': 'Laboratorios Implementacion Odoo',
     "data": [
             "security/ir.model.access.csv",
+            "views/estate_property_type_views.xml",
             "views/estate_property_views.xml",
             "views/estate_menus.xml",
     ],
