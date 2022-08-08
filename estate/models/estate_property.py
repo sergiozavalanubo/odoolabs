cb4503d97b5345ddb763095f946ec7fa0865aec2
Sergio Zavala <sergio.zavala@nubosoft.com>
2022-08-08 22:26:02 +0000

    Identity Info

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -53,3 +53,7 @@ class EstateProperty(models.Model):
     # Many2One
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
 
+    # Identity Info
+    user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="Buyer")
+
