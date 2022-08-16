ddf41e167254778a431b3406ac141d05aa947cc3
Sergio Zavala <sergio.zavala@nubosoft.com>
2022-08-16 01:08:33 +0000

    PropertyType +Sequence Widget

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,10 +5,11 @@ class EstatePropertyType(models.Model):
 
     _name = "estate.property.type"
     _description = "Real Estate Property Type"
-    _order = "name"
+    _order = "sequence, name"
     
     # Fields
     name = fields.Char("Name", required=True)
+    sequence = fields.Integer("Sequence", default=10)
     
     # One2Many
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
